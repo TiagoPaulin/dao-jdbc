4fc46dae6ad5d2d0f6940344a6fdcc25d3b43fe1
TiagoPaulin <thiago.vmp15@gmail.com>
2024-08-15 15:52:11 -0300

    insert implementation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -36,5 +36,11 @@ public class Main {
 
         }
 
+        System.out.println();
+        System.out.println("=== TEST 4: insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, new Department(2, null));
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! new Id = " + newSeller.getId());
+
     }
 }
